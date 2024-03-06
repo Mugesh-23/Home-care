e3b9e7c6718f18aa3918b6f9a3b7da9f18f50dec
Mugesh-23 <119048036+Mugesh-23@users.noreply.github.com>
2024-03-06 14:55:00 +0530

    Delete FrontEnd/DAY7 directory

--- FrontEnd/DAY7/src/main/java/com/elderly/elderly/enumerated/Role.java
+++
@@ -1,6 +0,0 @@
-package com.elderly.elderly.enumerated;
-
-
-public enum Role {
-    USER, ADMIN;
-}
--- FrontEnd/DAY7/src/main/java/com/elderly/elderly/repository/FeedbackRepo.java
+++
@@ -1,9 +0,0 @@
-package com.elderly.elderly.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.elderly.elderly.entity.Feedbackentity;
-
-public interface FeedbackRepo extends JpaRepository<Feedbackentity, Long> {
-
-}
